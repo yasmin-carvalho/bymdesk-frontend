dfc68ed9cdd88a3b4491af2d8c0bc08e18d08558
Yasmin Carvalho <yasminkarolynecarvalho@gmail.com>
2022-11-25 19:13:40 -0300

    feat: insert TabContainer in Matriculation

--- src/pages/Admin/components/Matriculation/index.tsx
+++ src/pages/Admin/components/Matriculation/index.tsx
@@ -1,23 +1,30 @@
 import { Button } from "../../../../components/Button";
+import { TabContainer } from "../../../../components/Tabs/styles";
 import { Main } from "../../styles";
 import { Container, Content, StyledInput, Text } from "./styles";
 
 export function Matriculation() {
   return (
-    <Main>
-      <Container>
-        <Text>Inscrição de Matrícula para Analistas</Text>
-        <StyledInput
-          label="Digite uma nova Matrícula"
-          required
-          mask="9999999999"
-        />
-        <StyledInput label="Confirme a Matrícula" required mask="9999999999" />
-      </Container>
-      <Content>
-        <Button>REGISTRAR</Button>
-        <Button>CANCELAR</Button>
-      </Content>
-    </Main>
+    <TabContainer>
+      <Main>
+        <Container>
+          <Text>Inscrição de Matrícula para Analistas</Text>
+          <StyledInput
+            label="Digite uma nova Matrícula"
+            required
+            mask="9999999999"
+          />
+          <StyledInput
+            label="Confirme a Matrícula"
+            required
+            mask="9999999999"
+          />
+        </Container>
+        <Content>
+          <Button>REGISTRAR</Button>
+          <Button>CANCELAR</Button>
+        </Content>
+      </Main>
+    </TabContainer>
   );
 }
