5a273ab02611784f9573ee8d99e57d839c5633ab
Ygor Salles Aniceto Carvalho <40208900+ygor-salles@users.noreply.github.com>
2022-12-04 21:42:47 -0300

    Feature/screen admin integration (#7)
    * feat: logic search
    
    * componentizando header-table
    
    * feature: screnn block
    
    * feat: screen location

--- src/pages/Admin/components/Block/index.tsx
+++ src/pages/Admin/components/Block/index.tsx
@@ -1,22 +1,54 @@
-import { Button } from "../../../../components/Button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
+import { FooterForm } from "../../../../components/FooterForm";
 import { TabContainer } from "../../../../components/Tabs/styles";
-import { Main } from "../../styles";
-import { Container, Content, StyledInput, Text } from "./styles";
+import {
+  fieldsBlock,
+  IFormBlockDTO,
+  schemaBlock,
+} from "../../../../dtos/IBlockDTO";
+import { useBlock } from "../../../../hooks/network/useBlock";
+import { Container, Form, StyledInput, Text } from "./styles";
 
 export function Block() {
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { isValid },
+  } = useForm<IFormBlockDTO>({
+    resolver: yupResolver(schemaBlock),
+    defaultValues: {
+      [fieldsBlock.BLOCO]: "",
+      [fieldsBlock.CONFIRME_BLOCO]: "",
+    },
+  });
+
+  const { onSubmit } = useBlock();
+
   return (
     <TabContainer>
-      <Main>
+      <Form
+        onSubmit={handleSubmit((data: IFormBlockDTO) => onSubmit(data, reset))}
+      >
         <Container>
           <Text>Inscrição de Bloco</Text>
-          <StyledInput label="Digite um novo Bloco" required />
-          <StyledInput label="Confirme o Bloco" required />
+          <StyledInput
+            label="Digite um novo Bloco"
+            required
+            name={fieldsBlock.BLOCO}
+            control={control}
+          />
+          <StyledInput
+            label="Confirme o Bloco"
+            required
+            name={fieldsBlock.CONFIRME_BLOCO}
+            control={control}
+          />
         </Container>
-        <Content>
-          <Button>REGISTRAR</Button>
-          <Button>CANCELAR</Button>
-        </Content>
-      </Main>
+        <FooterForm textButtonSubmit="REGISTRAR" disabled={!isValid} />
+      </Form>
     </TabContainer>
   );
 }
